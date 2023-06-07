ce30dfeddef3e40b0750007e0af08a0b18c2c7e4
crewd <newspaper321@nate.com>
2023-06-07 23:48:53 +0900

    fix: role moudle add controller, provider

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from 'src/store/store.entity';
-import { User } from 'src/user/user.decorator';
 import { Role } from './role.entity';
+import { User } from 'src/user/user.entity';
+import { RoleController } from './role.controller';
+import { RoleService } from './role.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role, Store, User])],
-  controllers: [],
-  providers: [],
+  controllers: [RoleController],
+  providers: [RoleService],
 })
 export class RoleModule {}
