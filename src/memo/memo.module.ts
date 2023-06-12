59e512a48e86332fd578f7aa4b49dd95dc50ecfc
crewd <newspaper321@nate.com>
2023-06-12 22:35:32 +0900

    feat: memo module에 controller, provider 추가

--- src/memo/memo.module.ts
+++ src/memo/memo.module.ts
@@ -4,10 +4,12 @@ import { Memo } from './memo.entity';
 import { User } from 'src/user/user.entity';
 import { Store } from 'src/store/store.entity';
 import { Role } from 'src/role/role.entity';
+import { MemoController } from './memo.controller';
+import { MemoService } from './memo.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Memo, User, Store, Role])],
-  controllers: [],
-  providers: [],
+  controllers: [MemoController],
+  providers: [MemoService],
 })
 export class MemoModule {}
