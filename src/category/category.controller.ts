ae432173d77b331ebaddcbb2b20097da0fb242a6
crewd <newspaper321@nate.com>
2023-06-14 01:40:29 +0900

    feat: create category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
   ParseIntPipe,
+  Post,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -14,6 +16,7 @@ import {
 import { CategoryService } from './category.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { User } from 'src/user/user.decorator';
+import { CreateCategorytDto } from './dto/create-category.dto';
 
 @ApiTags('category')
 @Controller()
@@ -34,5 +37,26 @@ export class CategoryController {
     @Param('storeId', ParseIntPipe) storeId: number,
   ) {
     return this.categoryService.getCategoryList(userId, storeId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('store/:storeId/category')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '카테고리 생성',
+    description: '카테고리 생성 API',
+  })
+  @ApiResponse({ status: 401, description: 'UnauthorizedException' })
+  @ApiResponse({ status: 404, description: 'NotFoundException' })
+  createCategory(
+    @User('userId', ParseIntPipe) userId: number,
+    @Param('storeId', ParseIntPipe) storeId: number,
+    @Body() createCategoryDto: CreateCategorytDto,
+  ) {
+    return this.categoryService.createCategory(
+      userId,
+      storeId,
+      createCategoryDto,
+    );
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { Repository } from 'typeorm';
@@ -6,6 +10,9 @@ import { Store } from 'src/store/store.entity';
 import { User } from 'src/user/user.entity';
 import { plainToInstance } from 'class-transformer';
 import { CategoryDto } from './dto/category.dto';
+import { CreateCategorytDto } from './dto/create-category.dto';
+import { Role } from 'src/role/role.entity';
+import { Permission } from 'src/role/role.enum';
 
 @Injectable()
 export class CategoryService {
@@ -18,6 +25,9 @@ export class CategoryService {
 
     @InjectRepository(User)
     private userRepository: Repository<User>,
+
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
   ) {}
 
   async getCategoryList(
@@ -44,5 +54,39 @@ export class CategoryService {
     const categoryList = plainToInstance(CategoryDto, categories);
 
     return categoryList;
+  }
+
+  async createCategory(
+    userId: number,
+    storeId: number,
+    createCategoryDto: CreateCategorytDto,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({ id: userId });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const store = await this.storeRepository.findOne({
+      id: storeId,
+      userId: userId,
+    });
+
+    if (!store) {
+      throw new NotFoundException();
+    }
+
+    const role = await this.roleRepository.findOne({ userId, storeId });
+
+    if (role.permission !== Permission.ADMIN) {
+      throw new UnauthorizedException();
+    }
+
+    const category = new Category();
+    category.store = store;
+    category.storeId = storeId;
+    category.name = createCategoryDto.name;
+
+    await this.categoryRepository.save(category);
   }
 }
