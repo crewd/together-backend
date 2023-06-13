bd664d8df869cb2423b08e7a749a497b47b44fb7
crewd <newspaper321@nate.com>
2023-06-14 01:10:39 +0900

    feat: category list

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,9 +1,38 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CategoryService } from './category.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { User } from 'src/user/user.decorator';
 
 @ApiTags('category')
 @Controller()
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
+
+  @UseGuards(AuthGuard)
+  @Get('store/:storeId/category')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '카테고리 목록 조회',
+    description: '카테고리 목록 조회 API',
+  })
+  @ApiResponse({ status: 401, description: 'UnauthorizedException' })
+  @ApiResponse({ status: 404, description: 'NotFoundException' })
+  getCategoryList(
+    @User('userId', ParseIntPipe) userId: number,
+    @Param('storeId', ParseIntPipe) storeId: number,
+  ) {
+    return this.categoryService.getCategoryList(userId, storeId);
+  }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,16 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { Repository } from 'typeorm';
 import { Store } from 'src/store/store.entity';
+import { User } from 'src/user/user.entity';
+import { plainToInstance } from 'class-transformer';
+import { CategoryDto } from './dto/category.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectRepository(Category)
-    private CategoryRepository: Repository<Category>,
+    private categoryRepository: Repository<Category>,
 
     @InjectRepository(Store)
     private storeRepository: Repository<Store>,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
+
+  async getCategoryList(
+    userId: number,
+    storeId: number,
+  ): Promise<CategoryDto[]> {
+    const user = await this.userRepository.findOne({ id: userId });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const store = await this.storeRepository.findOne({
+      id: storeId,
+      userId: userId,
+    });
+
+    if (!store) {
+      throw new NotFoundException();
+    }
+
+    const categories = await this.categoryRepository.find({ storeId: storeId });
+
+    const categoryList = plainToInstance(CategoryDto, categories);
+
+    return categoryList;
+  }
 }
