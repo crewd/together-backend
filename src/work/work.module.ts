5deaf08ebb2af564ce67adac43a13d235d483825
crewd <newspaper321@nate.com>
2023-06-15 02:08:07 +0900

    feat: work service

--- src/work/work.module.ts
+++ src/work/work.module.ts
@@ -5,10 +5,11 @@ import { Category } from 'src/category/category.entity';
 import { Store } from 'src/store/store.entity';
 import { Role } from 'src/role/role.entity';
 import { User } from 'src/user/user.entity';
+import { WokrService } from './work.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Work, Category, Store, User, Role])],
   controllers: [],
-  providers: [],
+  providers: [WokrService],
 })
 export class WorkModule {}
