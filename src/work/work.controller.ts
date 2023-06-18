d7f805faacded6ed06171cfea32454e5e18892cb
crewd <newspaper321@nate.com>
2023-06-19 02:38:45 +0900

    feat: create work controller

--- src/work/work.controller.ts
+++ src/work/work.controller.ts
@@ -1,8 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
   ParseIntPipe,
+  Post,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -14,6 +16,7 @@ import {
 import { WokrService } from './work.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { User } from 'src/user/user.decorator';
+import { CreateWorkDto } from './dto/create-work.dto';
 
 @ApiTags('업무사항')
 @Controller()
@@ -35,5 +38,28 @@ export class WorkController {
     @Param('categoryId', ParseIntPipe) categoryId: number,
   ) {
     return this.workService.getWorkList(userId, storeId, categoryId);
+  }
+
+  @Post('store/:storeId/category/:categoryId/work')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '업무 생성',
+    description: '업무 생성 API',
+  })
+  @ApiResponse({ status: 401, description: 'UnauthorizedException' })
+  @ApiResponse({ status: 404, description: 'NotFoundException' })
+  @ApiResponse({ status: 400, description: 'BadRequestException' })
+  createWork(
+    @User('userId', ParseIntPipe) userId: number,
+    @Param('storeId', ParseIntPipe) storeId: number,
+    @Param('categoryId', ParseIntPipe) categoryId: number,
+    @Body() createWorkDto: CreateWorkDto,
+  ) {
+    return this.workService.createWork(
+      userId,
+      storeId,
+      categoryId,
+      createWorkDto,
+    );
   }
 }
