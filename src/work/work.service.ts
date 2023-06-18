5bd6350945511c014c4c91d592462dc1a2612b83
crewd <newspaper321@nate.com>
2023-06-19 02:35:20 +0900

    feat: create work service

--- src/work/work.service.ts
+++ src/work/work.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Work } from './work.entity';
 import { Repository } from 'typeorm';
@@ -7,6 +12,9 @@ import { plainToInstance } from 'class-transformer';
 import { WorkDto } from './dto/work.dto';
 import { Category } from 'src/category/category.entity';
 import { User } from 'src/user/user.entity';
+import { CreateWorkDto } from './dto/create-work.dto';
+import { Role } from 'src/role/role.entity';
+import { Permission } from 'src/role/role.enum';
 
 @Injectable()
 export class WokrService {
@@ -22,6 +30,9 @@ export class WokrService {
 
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>,
+
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
   ) {}
 
   async getWorkList(
@@ -59,5 +70,49 @@ export class WokrService {
 
     const workList: WorkDto[] = plainToInstance(WorkDto, works);
     return workList;
+  }
+
+  async createWork(
+    userId: number,
+    storeId: number,
+    categoryId: number,
+    createWorkDto: CreateWorkDto,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({ id: userId });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const store = await this.storeRepository.findOne({
+      id: storeId,
+      userId: userId,
+    });
+
+    if (!store) {
+      throw new NotFoundException();
+    }
+
+    const category = await this.categoryRepository.findOne({ id: categoryId });
+
+    if (category.storeId !== storeId) {
+      throw new BadRequestException();
+    }
+
+    const role = await this.roleRepository.findOne({ userId, storeId });
+
+    if (role.permission !== Permission.ADMIN) {
+      throw new UnauthorizedException();
+    }
+
+    const work = new Work();
+    work.store = store;
+    work.storeId = storeId;
+    work.category = category;
+    work.categoryId = categoryId;
+    work.title = createWorkDto.title;
+    work.content = createWorkDto.content;
+
+    await this.workRepository.save(work);
   }
 }
